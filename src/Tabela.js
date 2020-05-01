76639bfeec2b16993acb98b0affa4bbc5454beb5
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-30 23:35:13 -0300

    modularizando tbody modularizando para facilitar manutenção

--- src/Tabela.js
+++ src/Tabela.js
@@ -13,31 +13,39 @@ const TableHead = () => {
     );
 }
 
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Paulo</td>
+                <td>React</td>
+                <td>1000</td>
+                <td><button>Remover</button></td>
+            </tr>
+            <tr>
+                <td>Nico</td>
+                <td>React</td>
+                <td>1000</td>
+                <td><button>Remover</button></td>
+            </tr>
+            <tr>
+                <td>Daniel</td>
+                <td>React</td>
+                <td>1000</td>
+                <td><button>Remover</button></td>
+            </tr>
+        </tbody>
+
+    );
+
+}
+
 class Tabela extends Component {
     render() {
         return (
             <table>
                 <TableHead/>
-                <tbody>
-                    <tr>
-                        <td>Paulo</td>
-                        <td>React</td>
-                        <td>1000</td>
-                        <td><button>Remover</button></td>
-                    </tr>
-                    <tr>
-                        <td>Nico</td>
-                        <td>React</td>
-                        <td>1000</td>
-                        <td><button>Remover</button></td>
-                    </tr>
-                    <tr>
-                        <td>Daniel</td>
-                        <td>React</td>
-                        <td>1000</td>
-                        <td><button>Remover</button></td>
-                    </tr>
-                </tbody>
+                <TableBody/>
             </table>
 
         );
