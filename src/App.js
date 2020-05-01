293142c94148f63be9fae9429b635cb1d56f7f0a
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-30 23:47:00 -0300

    transformando elemento App em classe

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Tabela from './Tabela';
 
-function App() {
-  const autores = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    }
-  ];
-  return (
-    <div className="App">
-      <Tabela autores = { autores }/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '9999'
+      }
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores={ this.state.autores } />
+      </div>
+    );
+  }
 }
 
 export default App;
