42ce89fd22dff33d1e58d1a1a18cd78985922494
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-30 23:32:34 -0300

    Modularizando tabela para usar em diversos lugares

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react';
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Autores</th>
-            <th>Livros</th>
-            <th>Preços</th>
-            <th>Remover</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Paulo</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-          <tr>
-            <td>Nico</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-          <tr>
-            <td>Daniel</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-        </tbody>
-      </table>
+      <Tabela/>
     </div>
   );
 }
