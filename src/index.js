f93c8d5e31a3ce7c208b2ce274e05223008ded3e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-20 11:20:34 -0300

    criadas rotas do projeto

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={App} />
+      <Route path='/sobre' component={Sobre} />
+      <Route path='/livros' component={Livros} />
+      <Route path='/autores' component={Autores} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
