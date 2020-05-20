acae01e41b44e4e6c40bdcbb18195ff82d18b690
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-19 23:25:54 -0300

    inserindo objeto de validação com as especificações

--- src/FormValidator.js
+++ src/FormValidator.js
@@ -1,6 +1,10 @@
 import validator from 'validator';
 
 class FormValidator{
+
+    constructor(validacao) {
+        this.validacao = validacao;
+    }
     
     valida(state){
         console.log('validado');
